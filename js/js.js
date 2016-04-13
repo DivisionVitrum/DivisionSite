60448730ac94fa9a8aa2aee27ff9bfe1e4e628e4
RoySijnesael <roysijnesael@gmail.com>
2016-04-13 21:05:04 +0200

    Changes to hamburger menu
    Deleted some stuff

--- js/js.js
+++ js/js.js
@@ -48,10 +48,3 @@ $(function() {
         menu.slideToggle();
     });
 });
-
-$(window).resize(function(){
-    var w = $(window).width();
-    if(w > 320 && menu.is(':hidden')) {
-        menu.removeAttr('style');
-    }
-}); 
