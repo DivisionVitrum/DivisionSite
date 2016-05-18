d38624f72f83cb24f467a9a7d87d15cbfa2e82e9
Niels <thisisdefenetlycrap@hotmail.com>
2016-05-18 12:53:48 +0200

    Blog update + fixed nav icon

--- js/js.js
+++ js/js.js
@@ -1,14 +1,6 @@
 window.addEventListener('load', init);
 
-$('a[href^="#"]').on('click', function(event) {
-    var target = $( $(this).attr('href') );
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: target.offset().top
-        }, 1000);
-    }
-});
+
 
 function init() {
     loop();
@@ -55,4 +47,31 @@ $(function() {
         menu.slideToggle(300);
         icon.toggleClass('open');
     });
+});
+///hover active
+var sections = $('section');
+var nav = $('nav');
+var nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+    var cur_pos = $(this).scrollTop();
+
+    sections.each(function() {
+        var top = $(this).offset().top - nav_height,
+            bottom = top + $(this).outerHeight();
+
+        if (cur_pos >= top && cur_pos <= bottom) {
+            nav.find('a').removeClass('active');
+            nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+        }
+    });
+});
+nav.find('a').on('click', function () {
+    var $el = $(this);
+    var id = $el.attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(id).offset().top - nav_height
+    }, 1000);
+    return false;
 });
