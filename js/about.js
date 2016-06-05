a100c20db07d97f0857df40f22f849728ea1e5f5
Roy Sijnesael <roysijnesael@gmail.com>
2016-06-05 22:28:50 +0200

    About update

--- js/about.js
+++ js/about.js
@@ -7,8 +7,9 @@ function Niels() {
     document.getElementById("back").style.display="flex";
 
     //Toont tekst
-    document.getElementById("n bio").innerHTML = "Hey!";
-    document.getElementById("n bio").style.display = "block";
+    document.getElementById("nbio").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet nibh nisl. Phasellus auctor nulla id lectus finibus, sit amet iaculis est tristique. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean orci nulla, fringilla ut tortor sit amet, ornare tincidunt est. Phasellus ullamcorper dictum leo, eu gravida elit aliquet at. Vestibulum ut velit lectus. Etiam interdum convallis lorem sed dictum. Duis maximus massa in lorem ornare, id pretium risus blandit. Cras at eros quis nibh aliquam suscipit quis id massa. Morbi rhoncus rhoncus lorem in gravida. Nunc eget aliquam sem, ut egestas lectus. Nunc sit amet tempus purus, eget venenatis justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.!";
+    document.getElementById("nquote").innerHTML = "Intuition fears dimensionless silence";
+    document.getElementById("nbio").style.display = "block";
 }
 
 function Roy() {
@@ -17,8 +18,9 @@ function Roy() {
 
     document.getElementById("back1").style.display="flex";
 
-    document.getElementById("r bio").innerHTML = "Hey!";
-    document.getElementById("r bio").style.display = "block";
+    document.getElementById("rbio").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet nibh nisl. Phasellus auctor nulla id lectus finibus, sit amet iaculis est tristique. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean orci nulla, fringilla ut tortor sit amet, ornare tincidunt est. Phasellus ullamcorper dictum leo, eu gravida elit aliquet at. Vestibulum ut velit lectus. Etiam interdum convallis lorem sed dictum. Duis maximus massa in lorem ornare, id pretium risus blandit. Cras at eros quis nibh aliquam suscipit quis id massa. Morbi rhoncus rhoncus lorem in gravida. Nunc eget aliquam sem, ut egestas lectus. Nunc sit amet tempus purus, eget venenatis justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.";
+    document.getElementById("rquote").innerHTML = "Intuition fears dimensionless silence";
+    document.getElementById("rbio").style.display = "block";
 }
 
 function Levi() {
@@ -27,23 +29,27 @@ function Levi() {
 
     document.getElementById("back2").style.display="flex";
 
-    document.getElementById("l bio").innerHTML = "Hey!"
-    document.getElementById("l bio").style.display = "block";
+    document.getElementById("lbio").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet nibh nisl. Phasellus auctor nulla id lectus finibus, sit amet iaculis est tristique. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean orci nulla, fringilla ut tortor sit amet, ornare tincidunt est. Phasellus ullamcorper dictum leo, eu gravida elit aliquet at. Vestibulum ut velit lectus. Etiam interdum convallis lorem sed dictum. Duis maximus massa in lorem ornare, id pretium risus blandit. Cras at eros quis nibh aliquam suscipit quis id massa. Morbi rhoncus rhoncus lorem in gravida. Nunc eget aliquam sem, ut egestas lectus. Nunc sit amet tempus purus, eget venenatis justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+    document.getElementById("lquote").innerHTML = "Intuition fears dimensionless silence";   
+    document.getElementById("lbio").style.display = "block";
 }
 
 function Back() { // Zet alles weer terug
 	// Niels
     document.getElementById("niels").style.display="flex";
-    document.getElementById("n bio").style.display="none";
+    document.getElementById("nbio").style.display="none";
+    document.getElementById("nquote").style.display="none";
     document.getElementById("back").style.display="none";
 
     // Roy
 	document.getElementById("roy").style.display="flex";
-    document.getElementById("r bio").style.display="none";
+    document.getElementById("rbio").style.display="none";
+    document.getElementById("rquote").style.display="none";
     document.getElementById("back1").style.display="none";
 
     // Levi
 	document.getElementById("levi").style.display="flex";
-    document.getElementById("l bio").style.display="none";
+    document.getElementById("lbio").style.display="none";
+    document.getElementById("lquote").style.display="none";
     document.getElementById("back2").style.display="none";
 }
