a7966758154834360f66af7a0caed668a6103b78
Roy Sijnesael <roysijnesael@145-24-200-90.wlan.hro.nl>
2016-06-06 10:31:55 +0200

    About fixed

--- js/about.js
+++ js/about.js
@@ -8,7 +8,10 @@ function Niels() {
 
     //Toont tekst
     document.getElementById("nbio").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet nibh nisl. Phasellus auctor nulla id lectus finibus, sit amet iaculis est tristique. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean orci nulla, fringilla ut tortor sit amet, ornare tincidunt est. Phasellus ullamcorper dictum leo, eu gravida elit aliquet at. Vestibulum ut velit lectus. Etiam interdum convallis lorem sed dictum. Duis maximus massa in lorem ornare, id pretium risus blandit. Cras at eros quis nibh aliquam suscipit quis id massa. Morbi rhoncus rhoncus lorem in gravida. Nunc eget aliquam sem, ut egestas lectus. Nunc sit amet tempus purus, eget venenatis justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.!";
+    
     document.getElementById("nquote").innerHTML = "Intuition fears dimensionless silence";
+    
+    document.getElementById("nquote").style.display = "block";
     document.getElementById("nbio").style.display = "block";
 }
 
@@ -19,7 +22,10 @@ function Roy() {
     document.getElementById("back1").style.display="flex";
 
     document.getElementById("rbio").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet nibh nisl. Phasellus auctor nulla id lectus finibus, sit amet iaculis est tristique. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean orci nulla, fringilla ut tortor sit amet, ornare tincidunt est. Phasellus ullamcorper dictum leo, eu gravida elit aliquet at. Vestibulum ut velit lectus. Etiam interdum convallis lorem sed dictum. Duis maximus massa in lorem ornare, id pretium risus blandit. Cras at eros quis nibh aliquam suscipit quis id massa. Morbi rhoncus rhoncus lorem in gravida. Nunc eget aliquam sem, ut egestas lectus. Nunc sit amet tempus purus, eget venenatis justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.";
+    
     document.getElementById("rquote").innerHTML = "Intuition fears dimensionless silence";
+    
+    document.getElementById("rquote").style.display = "block";
     document.getElementById("rbio").style.display = "block";
 }
 
@@ -30,7 +36,10 @@ function Levi() {
     document.getElementById("back2").style.display="flex";
 
     document.getElementById("lbio").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet nibh nisl. Phasellus auctor nulla id lectus finibus, sit amet iaculis est tristique. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean orci nulla, fringilla ut tortor sit amet, ornare tincidunt est. Phasellus ullamcorper dictum leo, eu gravida elit aliquet at. Vestibulum ut velit lectus. Etiam interdum convallis lorem sed dictum. Duis maximus massa in lorem ornare, id pretium risus blandit. Cras at eros quis nibh aliquam suscipit quis id massa. Morbi rhoncus rhoncus lorem in gravida. Nunc eget aliquam sem, ut egestas lectus. Nunc sit amet tempus purus, eget venenatis justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+    
     document.getElementById("lquote").innerHTML = "Intuition fears dimensionless silence";   
+    
+    document.getElementById("lquote").style.display = "block";
     document.getElementById("lbio").style.display = "block";
 }
 
