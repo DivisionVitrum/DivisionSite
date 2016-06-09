2483ed5b075cdf944290c726f89dc87371b90f71
onemoremac <levilaaper@gmail.com>
2016-06-09 14:13:36 +0200

    Eigen Tekst
    Mijn beschrijving bitches

--- js/about.js
+++ js/about.js
@@ -38,9 +38,9 @@ function Levi() {
 
     document.getElementById("back2").style.display="flex";
 
-    document.getElementById("lbio").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet nibh nisl. Phasellus auctor nulla id lectus finibus, sit amet iaculis est tristique. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean orci nulla, fringilla ut tortor sit amet, ornare tincidunt est. Phasellus ullamcorper dictum leo, eu gravida elit aliquet at. Vestibulum ut velit lectus. Etiam interdum convallis lorem sed dictum. Duis maximus massa in lorem ornare, id pretium risus blandit. Cras at eros quis nibh aliquam suscipit quis id massa. Morbi rhoncus rhoncus lorem in gravida. Nunc eget aliquam sem, ut egestas lectus. Nunc sit amet tempus purus, eget venenatis justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+    document.getElementById("lbio").innerHTML = "Ik ben Levi Laaper, ik ben 19 jaar en ik ben eerste jaar Creative Media and Game Technology student aan de hogeschool Rotterdam. Mijn ambitie is om out of the box te blijven denken en creative en vernieuwende technologiën toe passen in gave projecten. Ik waardeer open en eerlijkheid in een gesprek en probeer altijd vanuit een developers mindset na te denken over de mogelijkheden."
     
-    document.getElementById("lquote").innerHTML = "Intuition fears dimensionless silence";   
+    document.getElementById("lquote").innerHTML = "Een uitdaging betekend vernieuwing.";
     
     document.getElementById("lquote").style.display = "block";
     document.getElementById("lbio").style.display = "block";
