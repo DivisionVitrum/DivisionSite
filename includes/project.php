fc304fb705827070099c0f8f8f46022bf71ee6d5
onemoremac <levilaaper@gmail.com>
2016-09-07 17:56:28 +0200

    Tekst bij projecten over marloes

--- includes/project.php
+++ includes/project.php
@@ -8,9 +8,12 @@
                 <h1>Projecten</h1>
                 <sub></sub>
             </div>
-            <h2>Left</h2>
-            <p>Hier komt iets te staan over onze projecten. Eer komt een uitgelicht project in de vorm van een plaatje</p>
-            <a href="projects/"><button type="button" name="button">Bekijk al onze projecten</button></a>
+            <h2>Voet en Los</h2>
+            <p>Voet en Los was ons eerste project, nu we achteraf terugkijken zijn we in een half jaar tijd ver gekomen en hebben we veel kennis opgedaan.
+            </p>
+            <a href="http://www.voetenlos.nl/" target="_blank">
+                <button type="button" name="button">Bekijk de website</button>
+            </a>
         </div>
     </div>
 </aside>
